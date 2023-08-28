67d230e6afc6aa17ee28165ceb31c53dd8d767c3
zpjayyy <jay.lichzp@gmail.com>
2023-08-28 10:19:28 +0800

    feat: game

--- src/App.tsx
+++ src/App.tsx
@@ -104,14 +104,34 @@ function Board(props: {xIsNext: boolean, squares: string[], onPlay: any}) {
 }
 
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true)
-    const [history, setHistory] = useState(Array(9).fill(null))
-    const currentSquares = history[history.length - 1]
+    const [history, setHistory] = useState([Array(9).fill(null)])
+    const [currentMove, setCurrentMove] = useState(0)
+    const currentSquares = history[currentMove]
+    const xIsNext = currentMove % 2 ===0
 
-    function handlePlay(nextSquares?: string[]) {
-        setHistory([...history, nextSquares])
-        setXIsNext(!xIsNext)
+    function handlePlay(nextSquares: string[]) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+        setHistory(nextHistory)
+        setCurrentMove(nextHistory.length - 1)
     }
+
+    function jumpTo(nextMove: number) {
+        setCurrentMove(nextMove)
+    }
+
+    const moves = history.map((squares, move) => {
+        let description
+        if (move > 0) {
+            description = "go to move #" + move
+        } else {
+            description = "go to game start"
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        )
+    })
 
     return (
         <div className="game">
@@ -119,7 +139,7 @@ export default function Game() {
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
             </div>
             <div className="game-info">
-                <ol>{/*todo*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     )
